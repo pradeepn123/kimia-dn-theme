862570d2f40abd839eb1b34f99902b711c78d7f0
Brijesh Patil <brijesh@shoptrade.co>
2024-03-15 11:35:18 +0530

    subscription integration in product page

--- src/js/components/react/subscription-selector.js
+++ src/js/components/react/subscription-selector.js
@@ -1,46 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { onMounted } from "vue";
 import Onetime from "./onetime";
 import Subscription from "./subscription";
 
+const fetchData = () => {
+    const sellingPlans = document.querySelectorAll("input[data-radio-type='selling_plan']");
+    return Array.from(sellingPlans).map(sellingPlan => {
+        const sellingPlansInput = sellingPlan.dataset;
+        const sellingId = sellingPlansInput.sellingPlanId;
+        const variantCAP = sellingPlansInput.variantCompareAtPrice.split(" ");
+        const variantCompareAtPrice = variantCAP[0];
+        const variantP = sellingPlansInput.variantPrice.split(" ");
+        const variantPrice = variantP[0];
+        const parent = sellingPlan.closest("label");
+        const labelText = parent.textContent.split(", ");
+        const labelDeliveryText = labelText[0];
+        const labelDeliveryOffer = labelText[1].split(" ");
+        const sellingPlanOffer = labelDeliveryOffer[0];
+        return {
+            sellingId,
+            variantCompareAtPrice,
+            variantPrice,
+            sellingPlanText: labelDeliveryText,
+            sellingPlanOffer
+        };
+    });
+};
+
 export default () => {
     const [data, updateData] = useState([]);
+
     useEffect(() => {
-        let data = [];
-        document.querySelectorAll(("input[data-radio-type='selling_plan']")).forEach((sellingPlan)=>{
-            
-            const sellingPlansInput = sellingPlan.dataset;
-            // console.log(sellingPlansInput);
-            const sellingId = sellingPlansInput.sellingPlanId;
-            const variantCompareAtPrice = sellingPlansInput.variantCompareAtPrice;
-            const variantPrice = sellingPlansInput.variantPrice;
-            const parent = sellingPlan.closest("label");
-            const labelText = parent.textContent.split(", ");
-            // console.log(labelText, "-----labelText");
-            const labelDeliveryText = labelText[0];
-            const labelDeliveryOffer = labelText[1].split(" ");
-            const sellingPlanOffer = labelDeliveryOffer[0];
-            console.log(labelDeliveryText, "-----labelDeliveryText");
-            console.log(sellingPlanOffer, "-----sellingPlanOffer");
+        const newData = fetchData();
+        updateData(prevData => [...prevData, ...newData]);
+    }, []);
 
-            const dataObj = {
-                sellingId: sellingId,
-                variantCompareAtPrice: variantCompareAtPrice,
-                variantPrice: variantPrice,
-                sellingPlanText: labelDeliveryText,
-                sellingPlanOffer: sellingPlanOffer
-            };
-
-            data.push(dataObj);
-        });
-
-        updateData(data);
-        console.log(data, "JSON data");
-    },[])
-    return(
+    return (
         <>
             <Subscription data={data} />
             <Onetime />
         </>
-    )
-}
+    );
+};
--- src/js/components/react/subscription.js
+++ src/js/components/react/subscription.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'StyleComponents/subscription-sec.scss';
 
-const subscriptionContainer = (props) => {
-    const data = props.data;
-    return(
-        <>
-            <div className="subscription-container">
-                <p className="subscription-container__value-tag">Best value</p>
-                <div className="subscription-container__subs-one-wrapper">
-                    <div className="subscription-container__subscription-wrapper">
-                        <div className="subscription-container__subs-text">
-                        <input type="radio" id="subscribeSave" name="purchase" value="sellingIdsubs"/>
-                        <label htmlFor="subscribeSave">SUBSCRIBE & SAVE 10%</label><br/>
-                        <div className="subscription-container__dropdown">
-                            <select name="delivery" id="interval" className="subscription-container__dropbtn" defaultValue="DeliveryOne">
-                            {data.map((item) => (
-                                <option value={item.sellingPlanText}>{item.sellingPlanText}</option>
-                            ))};
-                            </select>
-                        </div>
-                        </div>
-                        <div className="subscription-container__subs-price">
-                        <p className="subscription-container__subs-compPrice">$35.00</p>
-                        <p className="subscription-container__subs-actualPrice">$30.00</p>
-                        </div>
+const SubscriptionContainer = (props) => {
+    const [subscription, setSubscription] = useState(null);
+
+    useEffect(() => {
+        if (props.data && props.data.length > 0) {
+            const initialOption = props.data[0];
+            setSubscription(initialOption);
+        }
+    }, [props.data]);
+
+    const handleSelectChange = (event) => {
+        const selectedOption = event.target.value;
+        const selectedOptionObject = props.data.find(item => item.sellingId === selectedOption);
+        setSubscription(selectedOptionObject);
+    };
+
+    return (
+        <div className="subscription-container">
+            <p className="subscription-container__value-tag">Best value</p>
+            <div className="subscription-container__subs-one-wrapper">
+                <div className="subscription-container__subscription-wrapper">
+                    <div className="subscription-container__subs-text">
+                        <input type="radio" id="subscribeSave" name="purchase" value="sellingIdsubs" />
+                        <label htmlFor="subscribeSave">
+                            <span className="subscribeSave__text">SUBSCRIBE & SAVE {subscription?.sellingPlanOffer || ''}</span>
+                            <div className="subscription-container__dropdown">
+                                <select name="delivery" id="interval" className="subscription-container__dropbtn" defaultValue="" onChange={handleSelectChange}>
+                                    {props.data && props.data.map((item, index) => (
+                                        <option key={index} value={item.sellingId}>{item.sellingPlanText}</option>
+                                    ))}
+                                </select>
+                            </div>
+                        </label>
+                    </div>
+                    <div className="subscription-container__subs-price">
+                        {subscription && (
+                            <>
+                                <p className="subscription-container__subs-compPrice">{subscription.variantCompareAtPrice}</p>
+                                <p className="subscription-container__subs-actualPrice">{subscription.variantPrice}</p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
-export default subscriptionContainer;
+        </div>
+    );
+};
 
+export default SubscriptionContainer;
